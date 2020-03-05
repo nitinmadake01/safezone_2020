8f0515bbd324dc52514584b13fe40c46288a7c88
nitin <nitinmadake01@gmail.com>
2020-03-05 22:41:08 +0530

    local storage code :variations

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
+  publicData: any;
+  showGallery:boolean;
 
-  constructor() { }
+  constructor(
+    private _HttpClient: HttpClient
+  ) { }
 
   ngOnInit() {
+    if(localStorage.getItem('authStatus') === "200"){
+      this.showGallery = true;
+    }
+    console.log("homepage",localStorage.getItem('authStatus'))
+    this._HttpClient.get("/assets/publicData.json").subscribe(data => {
+      let Data: any = data;
+      this.publicData = Data.images;
+      console.log(this.publicData, "public s")
+    })
+  }
+
+  logout(){
+    alert('hi');
+    let key="authStatus"
+    localStorage.setItem(key, "404");
+  } 
+  ngOnDestroy(){
+    console.log("HomepageComponent destroy");
   }
 
 }
